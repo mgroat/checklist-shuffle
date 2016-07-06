8a3de4ae2ab633b59f108dba5d6d2c30220d31e6
Matthew Groat <mgroat@domain.local>
2016-07-06 11:06:34 -0400

    Fix getting env variable

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$dbopts = parse_url(getenv(['DATABASE_URL']));
+$dbopts = parse_url(getenv('DATABASE_URL'));
 $conn = pg_connect("host={$dbopts['host']} port={$dbopts['port']} dbname=".ltrim($dbopts['path'],'/')." user={$dbopts['user']} password={$dbopts['pass']}");
 $sql = pg_prepare($conn,"SHOW","SELECT * FROM pg_catalog.pg_tables");
 $results = pg_execute($conn,"SHOW",[]);
