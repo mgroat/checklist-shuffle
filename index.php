ca258cf4739122d744aabaa4c2f0cdace998900f
Matthew Groat <mgroat@domain.local>
2016-07-06 10:59:38 -0400

    Connect to postgres

--- index.php
+++ index.php
@@ -1,2 +1,8 @@
 <?php
-echo "Hello World";
+include $_SERVER['DOCUMENT_ROOT'].'/ptgLib.php';
+$dbopts = parse_url(getenv(['DATABASE_URL']));
+$conn = pg_connect("host={$dbopts['host']} port={$dbopts['port']} dbname=".ltrim($dbopts['path'],'/')." user={$dbopts['user']} password={$dbopts['pass']}");
+$sql = pg_prepare($conn,"SHOW","SELECT * FROM pg_catalog.pg_tables");
+$results = pg_execute($conn,"SHOW",[]);
+$results = pg_fetch_all($results);
+debug($results);
