c40cfba8c7094ddd259fdbf58f7a578706928c7e
Matthew Groat <mgroat@gmail.com>
2016-07-07 20:50:46 -0400

    Remove filter for troubleshooting

--- get.php
+++ get.php
@@ -1,7 +1,7 @@
 <?php
 $dbopts = parse_url(getenv('DATABASE_URL'));
 $conn = pg_connect("host={$dbopts['host']} port={$dbopts['port']} dbname=".ltrim($dbopts['path'],'/')." user={$dbopts['user']} password={$dbopts['pass']}");
-$sql = pg_prepare($conn,"SHOW",'SELECT * FROM checkMaster WHERE parent = $1',[$_GET['parent']]);
-$results = pg_execute($conn,"SHOW",[]);
+$sql = pg_prepare($conn,"SHOW",'SELECT * FROM checkItem');
+$results = pg_execute($conn,"SHOW",[$_GET['parent']]);
 $results = pg_fetch_all($results);
 print_r($results);
