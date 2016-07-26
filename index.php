aac8ad322d65119cf862281dc6ca3bd41aa055e6
Matthew Groat <mgroat@gmail.com>
2016-07-25 20:06:02 -0400

    Workable demo, index links to it

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
-<?php
-$dbopts = parse_url(getenv('DATABASE_URL'));
-$conn = pg_connect("host={$dbopts['host']} port={$dbopts['port']} dbname=".ltrim($dbopts['path'],'/')." user={$dbopts['user']} password={$dbopts['pass']}");
-$sql = pg_prepare($conn,"SHOW","SELECT * FROM pg_catalog.pg_tables");
-$results = pg_execute($conn,"SHOW",[]);
-$results = pg_fetch_all($results);
-print_r($results);
+<h1>
+<a href='./check.html?parent=3'>Click here to see functional demo</a>
+</h1>
