9f70af61dab12b7bc5a202dbb82be847341e84e9
Matthew Groat <mgroat@gmail.com>
2016-09-26 19:45:40 -0400

    Fix dead link on index page

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <h1>
-	<a href='./check.html?parent=11'>Click here to see functional demo</a><br>
+	<a href='./check.html?11'>Click here to see functional demo</a><br>
 	<a href='./make.html'>Click here to make your own checklist</a>
 
 </h1>
