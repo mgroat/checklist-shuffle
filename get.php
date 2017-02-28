30e6e8b62a54424863c363491e0e76ec2ec5717d
Matthew Groat <mgroat@gmail.com>
2017-02-28 18:54:12 -0500

    Working with library

--- get.php
+++ get.php
@@ -1,11 +1,7 @@
 <?php
-$dbUrl = getenv('DATABASE_URL');
-if(file_exists('./conf.txt')) //This will exist on our test server, not on Heroku
-    $dbUrl = file_get_contents("./conf.txt");
-$dbopts = parse_url($dbUrl);
-$conn = pg_connect("host={$dbopts['host']} port={$dbopts['port']} dbname=".ltrim($dbopts['path'],'/')." user={$dbopts['user']} password={$dbopts['pass']}");
-$sql = pg_prepare($conn,"SHOW",'SELECT * FROM "checkItem" WHERE parent = $1');
-//$sql = pg_prepare($conn,"SHOW","SELECT * FROM pg_catalog.pg_tables");
-$results = pg_execute($conn,"SHOW",[$_GET['parent']]);
-$results = json_encode(pg_fetch_all($results));
+include 'library.php';
+$conn = pgConnect();
+$stmt = 'SELECT * FROM "checkItem" WHERE parent = $1';
+$results = pgQuery($conn,$stmt,[$_GET['parent']]);
+$results = json_encode($results);
 echo $results;
--- post.php
+++ post.php
@@ -1,29 +1,25 @@
 <?php
+include 'library.php';
 if(empty($_POST))
 	die("You should only come to this page when creating a new checklist");
-$dbUrl = getenv('DATABASE_URL');
-if(file_exists('./conf.txt')) //This will exist on our test server, not on Heroku
-    $dbUrl = file_get_contents("./conf.txt");
-$dbopts = parse_url($dbUrl);
-$conn = pg_connect("host={$dbopts['host']} port={$dbopts['port']} dbname=".ltrim($dbopts['path'],'/')." user={$dbopts['user']} password={$dbopts['pass']}");
+$conn = pgConnect();
 $stmt = 'INSERT INTO "checkMaster" (description, owner) VALUES ($1,$2) RETURNING pk ';
 $vals = [$_POST['description'],0];
-$sql = pg_prepare($conn,"INSERT-MASTER",$stmt);
-$results = pg_execute($conn,"INSERT-MASTER",$vals);
-$parent = pg_fetch_all($results)[0]['pk'];
+$results = pgQuery($conn,$stmt,$vals);
+$parent = $results[0]['pk'];
 
 $stmt = 'INSERT INTO "checkItem" (data, parent, depend, item) VALUES ($1, $2, $3, $4)';
-$sql = pg_prepare($conn,"INSERT-DETAIL",$stmt);
 foreach($_POST['data'] as $key => $data) {
 	if(!empty($data['detail'])) {
 		if(empty($data['depend']))
 			$data['depend'] = null;
 		$vals = [$data['detail'],$parent,$data['depend'],$key];
-		$results = pg_execute($conn,"INSERT-DETAIL",$vals);
+		$results = pgQuery($conn,$stmt,$vals);
 	}	
 }
-$url = $_SERVER['HTTP_ORIGIN']."/check.html?$parent";
+$absUrl = $_SERVER['HTTP_ORIGIN']."/check.html?$parent";
+$relUrl = "check.html?$parent";
 ?>
 
-<center><h1>Your super-secret URL is <a href="<?=$url?>"><?=$url?></a></h1></center>
+<center><h1>Your super-secret URL is <a href="<?=$relUrl?>"><?=$absUrl?></a></h1></center>
 <center><h2>Bookmark the above URL to come back to your checklist at any time</h2></center>
