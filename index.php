aceaaedf4dffdfcc1ed22d3f6208eec301575877
Matthew Groat <mgroat@gmail.com>
2017-03-28 19:42:59 -0400

    Pretty up some formatting

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
-<h1>
-	<a href='./check.html?DfsHQzYUvamIWAWr8Xaa'>Click here to see functional demo</a><br>
-	<a href='./make.html'>Click here to make your own checklist</a>
-
-</h1>
+<h1>Checklist Shuffle</h1>
+<h2><a href='./check.html?DfsHQzYUvamIWAWr8Xaa'>Click here to see functional demo</a></h2>
+<h2><a href='./make.html'>Click here to make your own checklist</a></h2>
